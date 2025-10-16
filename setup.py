d5dfa3a8e5ff70539e26aee980cb09ba5e7f0eb8
John Sirois <john.sirois@gmail.com>
2025-10-16 11:23:01 -0700

    Add 3.15 support. (#10)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def long_description():
 
 setup(
   name='p537',
-  version='1.0.8',
+  version='1.0.9',
   author="John Sirois",
   author_email="john.sirois@gmail.com",
   description='A tiny platform-specific distribution with a console script.',
@@ -31,8 +31,9 @@ setup(
     'Programming Language :: Python :: 3.12',
     'Programming Language :: Python :: 3.13',
     'Programming Language :: Python :: 3.14',
+    'Programming Language :: Python :: 3.14',
   ],
-  python_requires=">=3.6,<3.15",
+  python_requires=">=3.6,<3.16",
   ext_modules=[
     Extension('p537', sources=['p537module.c']),
   ],
